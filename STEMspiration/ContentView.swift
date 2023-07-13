1d8b59a56330fe72f4f61ef6427f9ee688728d2f
DaianaM <daiana.m.0708@gmail.com>
2023-07-12 21:29:57 -0700

    updated stuff

--- STEMspiration/ContentView.swift
+++ STEMspiration/ContentView.swift
@@ -9,13 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        
+        VStack(alignment: .leading, spacing: 20.0) {
+            Image("Joy")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(15)
+            
+            HStack {
+                Text("Joy Buolamwini :)")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                Text("Poet of Code")
+            }
+            
+            
+
+            
+            Text("Founder of the Algorithmic Justice Leage. Uses art and research to illuminate social implications of AI and to promote equitable and accountable technology.")
+            
+    
+            
+            
+            
         }
-        .padding()
+        
+        
+        
     }
 }
 
